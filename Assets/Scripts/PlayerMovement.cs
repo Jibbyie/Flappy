5bb28f5b2724aa7dd4acb42a24bf9e2afd39c5c9
Jibbyie <jibbyie@gmail.com>
2023-12-10 14:58:25 +0000

    Discord RP, Player Movement script

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float jumpForce;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponentInChildren<Rigidbody2D>();
+
+        if (rb == null)
+        {
+            Debug.LogError("Rigidbody2D not found on the player or its children.");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce) * Time.deltaTime;
+    }
+}
